745fa0ff4dd47c400583aa19f7ea7c4d001df2ad
codingkatty <m001720@permatapintar.ukm.edu.my>
2025-08-05 17:05:22 +0800

    :3

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,34 @@
+const manifest = chrome.runtime.getManifest()
+
+const url = new URL('https://accounts.google.com/o/oauth2/auth')
+
+url.searchParams.set('client_id', manifest.oauth2.client_id)
+url.searchParams.set('response_type', 'id_token')
+url.searchParams.set('access_type', 'offline')
+url.searchParams.set('redirect_uri', `https://${chrome.runtime.id}.chromiumapp.org`)
+url.searchParams.set('scope', manifest.oauth2.scopes.join(' '))
+
+chrome.identity.launchWebAuthFlow(
+  {
+    url: url.href,
+    interactive: true,
+  },
+  async (redirectedTo) => {
+    if (chrome.runtime.lastError) {
+      // auth was not successful
+    } else {
+      // auth was successful, extract the ID token from the redirectedTo URL
+      const url = new URL(redirectedTo)
+      const params = new URLSearchParams(url.hash)
+
+      const { data, error } = await supabase.auth.signInWithIdToken({
+        provider: 'google',
+        token: params.get('id_token'),
+      })
+    }
+  }
+)
+
 // vibecoded btw my first script did NOT work :wilted-flower
 function checkActiveTab() {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
